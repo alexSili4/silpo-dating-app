d87ae8d5357c6a742e1ee5e34c13c7a124b90761
Silich Alexander <167785662+alexSili4@users.noreply.github.com>
2025-01-24 08:00:00 +0200

    Initial commit

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import Container from '@/components/Container';
+import Section from '@/components/Section';
+import { FC } from 'react';
+
+const MainPage: FC = () => (
+  <Section>
+    <Container>
+      <p>main page</p>
+    </Container>
+  </Section>
+);
+
+export default MainPage;
